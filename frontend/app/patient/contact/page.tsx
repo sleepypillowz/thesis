a57494eae8f9f9e123429713325b66496fe8f33c
Alexander Acojido <alexacojido@gmail.com>
2025-02-25 16:15:24 +0800

    fixed dependency error

--- frontend/app/patient/contact/page.tsx
+++ frontend/app/patient/contact/page.tsx
@@ -4,14 +4,24 @@ export default function Page() {
       <h1 className="text-gray-foreground mb-4 text-3xl font-semibold">
         Contact Us
       </h1>
-      <h2>Socials</h2>
+      <h2 className="text-xl">Socials</h2>
       <div className="ms-2">
         <p>Facebook: https://www.facebook.com/malibiranmed</p>
       </div>
-      <h2>Mobile </h2>
+
+      <br />
+
+      <h2 className="text-xl">Mobile</h2>
       <div className="ms-2">
-        <h2>Globe: 0945 239 5382</h2>
-        <h2>Smart: 0999 820 5684</h2>
+        <p>Globe: 0945 239 5382</p>
+        <p>Smart: 0999 820 5684</p>
+      </div>
+
+      <br />
+
+      <h2 className="text-xl">Address</h2>
+      <div className="ms-2">
+        <p>130 Old Samson Road Balintawak Quezon City</p>
       </div>
     </div>
   );
