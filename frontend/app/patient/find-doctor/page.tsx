8de825b546523f32affa917dd6cd6269a608353d
Alexander Acojido <alexacojido@gmail.com>
2025-03-03 16:33:28 +0800

    update patient-find-doctor and admin-help
    made help and select component modular

--- frontend/app/patient/find-doctor/page.tsx
+++ frontend/app/patient/find-doctor/page.tsx
@@ -1,52 +1,96 @@
+import { MdOutlinePregnantWoman } from "react-icons/md";
+import { GiKidneys } from "react-icons/gi";
+import { Badge } from "@/components/ui/badge";
+import { SelectComponent } from "@/components/atoms/select-component";
+import { Baby, Ear, HeartPulse, PillBottle } from "lucide-react";
+
 export default function Page() {
+  const doctorTypes = [
+    "Pediatrician",
+    "OB-Gyne",
+    "ENT",
+    "Cardiologist",
+    "Internist",
+    "Nephrologist",
+  ];
+
   return (
     <div className="flex-1 px-8 py-8">
-
-      <section className="px-4 sm:px-6 lg:px-8">
-        <div className="mx-auto max-w-lg text-center">
-          <h2 className="text-3xl font-extrabold sm:text-4xl">
-            Find a Doctor
-          </h2>
-          <p className="mt-4 text-lg">
-            Search for doctors by name or specialty. Let us help you find the best healthcare professional for
-            your needs.
-          </p>
-        </div>
-
-        <div className="card mx-auto mt-12 max-w-lg rounded-xl">
-          <form action="#" method="GET">
-            <div className="grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-0">
-
-              <div className="sm:col-span-1">
-                <label htmlFor="doctor-name" className="block text-sm font-medium">Doctor Name</label>
-                <input type="text" id="doctor-name" name="doctor_name" placeholder="Search by name"
-                  className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" />
-              </div>
-
-              <div className="sm:col-span-1">
-                <label htmlFor="specialty" className="block text-sm font-medium">Specialty</label>
-                <select id="specialty" name="specialty"
-                  className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
-                  <option value="">Select a specialty</option>
-                  <option value="cardiology">Cardiology</option>
-                  <option value="neurology">Neurology</option>
-                  <option value="pediatrics">Pediatrics</option>
-                  <option value="orthopedics">Orthopedics</option>
-                  <option value="dermatology">Dermatology</option>
-                </select>
-              </div>
+      <h1 className="text-gray-foreground mb-4 text-3xl font-semibold">
+        Find a Doctor
+      </h1>
+      <SelectComponent title={"Select a Doctor Type..."} items={doctorTypes} />
+      <div className="m-4 grid grid-cols-1 place-items-center gap-4 text-card-foreground md:grid-cols-2 lg:grid-cols-4">
+        <div className="card block w-full max-w-sm space-y-2 rounded-xl">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center">
+              <Baby className="me-2 text-primary" />
+              <p>Pediatrician</p>
             </div>
-
-            <div className="text-center">
-              <button type="submit"
-                className="w-full rounded-lg bg-sky-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-sky-800 focus:outline-none focus:ring-4 focus:ring-sky-300">
-                Submit
-              </button>
-            </div>
-          </form>
+          </div>
+          <p>Dra. Bolima</p>
+          <Badge variant="outline">Tuesday</Badge>
+          <Badge variant="outline">Friday</Badge>
+          <Badge variant="outline">10:00AM - 12:00PM</Badge>
         </div>
-      </section>
-
+        <div className="card block w-full max-w-sm space-y-2 rounded-xl">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center">
+              <MdOutlinePregnantWoman className="me-2 text-primary" />
+              <p>OB-Gyne</p>
+            </div>
+          </div>
+          <p>Dra. Lee</p>
+          <Badge variant="outline">Tuesday</Badge>
+          <Badge variant="outline">Saturday</Badge>
+          <Badge variant="outline">10:00AM - 12:00PM</Badge>
+        </div>
+        <div className="card block w-full max-w-sm space-y-2 rounded-xl">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center">
+              <Ear className="me-2 text-primary" />
+              <p>ENT</p>
+            </div>
+          </div>
+          <p>Dr. Sy</p>
+          <Badge variant="outline">Wednesday</Badge>
+          <Badge variant="outline">Friday</Badge>
+          <Badge variant="outline">8:30AM - 10:30AM</Badge>
+        </div>
+        <div className="card block w-full max-w-sm space-y-2 rounded-xl">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center">
+              <HeartPulse className="me-2 text-primary" />
+              <p>Cardiologist</p>
+            </div>
+          </div>
+          <p>Dr. Co</p>
+          <Badge variant="outline">Monday</Badge>
+          <Badge variant="outline">10:00AM - 12:00PM</Badge>
+        </div>
+        <div className="card block w-full max-w-sm space-y-2 rounded-xl">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center">
+              <PillBottle className="me-2 text-primary" />
+              <p>Internal Medicine</p>
+            </div>
+          </div>
+          <p>Dra. Kat Sanchez</p>
+          <Badge variant="outline">Tuesday</Badge>
+          <Badge variant="outline">9:00AM - 11:00AM</Badge>
+        </div>
+        <div className="card block w-full max-w-sm space-y-2 rounded-xl">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center">
+              <GiKidneys className="me-2 text-primary" />
+              <p>Nephorologist</p>
+            </div>
+          </div>
+          <p>Dra. Kat Sanchez</p>
+          <Badge variant="outline">Thursday</Badge>
+          <Badge variant="outline">8:30AM - 10:30AM</Badge>
+        </div>
+      </div>
     </div>
   );
 }
