f3d7ce1a5d3a0265121a621e6567505142a0f195
Alexander Acojido <alexacojido@gmail.com>
2025-03-12 17:03:54 +0800

    refactor admin medicine
    added supabase test

--- frontend/components/molecules/tables/medicine-columns.tsx
+++ frontend/components/molecules/tables/medicine-columns.tsx
@@ -1,36 +1,34 @@
-"use client"
+"use client";
 
-import type { ColumnDef } from "@tanstack/react-table"
-import { Checkbox } from "@/components/ui/checkbox"
+import type { ColumnDef } from "@tanstack/react-table";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { MoreHorizontal } from "lucide-react";
+import Link from "next/link";
 
-import { Button } from "@/components/ui/button"
+export type Medicine = {
+  id: string;
+  name: string;
+  stocks: number;
+};
 
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
-import { MoreHorizontal } from "lucide-react"
-import { format } from 'date-fns';
-import { enGB } from 'date-fns/locale'
-import Link from "next/link"
-
-export type Patient = {
-  patient_id: string;
-  first_name: string;
-  middle_name: string;
-  last_name: string;
-  age: number;
-  queue_data: {
-    created_at: string;
-    status: string;
-  };
-  time: string;
-  complaint: string;
-}
-
-export const columns: ColumnDef<Patient>[] = [
+export const columns: ColumnDef<Medicine>[] = [
   {
     id: "select",
     header: ({ table }) => (
       <Checkbox
-        checked={table.getIsAllPageRowsSelected() || (table.getIsSomePageRowsSelected() && "indeterminate")}
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
         onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
         aria-label="Select all"
       />
@@ -46,62 +44,28 @@ export const columns: ColumnDef<Patient>[] = [
     enableHiding: false,
   },
   {
-    accessorKey: "no", // Column for No
-    header: "No",
+    accessorKey: "id",
+    header: "ID",
+  },
+  {
+    accessorKey: "name",
+    header: "Medicine Name",
+  },
+  {
+    accessorKey: "stocks",
+    header: "stocks",
     cell: ({ row }) => {
-      // Calculate the patient number based on the row index
-      const patientIndex = row.index + 1; // Add 1 to make it 1-based index
-      return patientIndex;
-    },
-  },
-  {
-    accessorKey: "patient_id",
-    header: "Patient ID",
-  },
-  {
-    id: "patient_name", // Use 'id' for a custom column
-    header: "Patient Name",
-    cell: ({ row }) => {
-      const { first_name, middle_name, last_name } = row.original; // Get data from the row
-      return `${first_name} ${middle_name ? middle_name : ""} ${last_name}`.trim(); // Combine the names
-    },
-  },
-  {
-    accessorKey: "age",
-    header: "Age",
-  },
-  {
-    id: "created_date", // Column for Created Date (foreign key)
-    header: "Created Date",
-    cell: ({ row }) => {
-      const createdAt = row.original.queue_data?.created_at;
-      return createdAt ? format(new Date(createdAt), "d MMMM yyyy", { locale: enGB }) : "N/A";
-    },
-  },
-  {
-    id: "time", // Column for Created Date (foreign key)
-    header: "Time",
-    cell: ({ row }) => {
-      const createdAt = row.original.queue_data?.created_at;
-      return createdAt ? format(new Date(createdAt), "HH:mm:ss", { locale: enGB }) : "N/A";
-    },
-  },
-  {
-    accessorKey: "complaint",
-    header: "Complaint",
-  },
-  {
-    id: "status", // Column for Created Date (foreign key)
-    header: "Status",
-    cell: ({ row }) => {
-      const status = row.original.queue_data?.status;
-      return status || "N/A";
+      return row.original.stocks > 0 ? (
+        row.original.stocks
+      ) : (
+        <span className="text-red-500">Out of stock</span>
+      );
     },
   },
   {
     id: "actions",
     cell: ({ row }) => {
-      const payment = row.original
+      const medicine = row.original;
 
       return (
         <DropdownMenu>
@@ -113,23 +77,22 @@ export const columns: ColumnDef<Patient>[] = [
           </DropdownMenuTrigger>
           <DropdownMenuContent align="end">
             <DropdownMenuLabel>Actions</DropdownMenuLabel>
-            <DropdownMenuItem onClick={() => navigator.clipboard.writeText(payment.patient_id)}>
-              Copy patient ID
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(medicine.id)}
+            >
+              Copy Medicine ID
             </DropdownMenuItem>
             <DropdownMenuSeparator />
-            <Link href="/admin/patient-information">
-            <DropdownMenuItem>View</DropdownMenuItem>
+            <Link href={`/admin/medicine/${medicine.id}`}>
+              <DropdownMenuItem>View</DropdownMenuItem>
             </Link>
-            <Link href="/admin/patient-information">
-            <DropdownMenuItem>Edit</DropdownMenuItem>
+            <Link href={`/admin/medicine/edit/${medicine.id}`}>
+              <DropdownMenuItem>Edit</DropdownMenuItem>
             </Link>
-            <Link href="/admin/patient-information">
-            <DropdownMenuItem>Delete</DropdownMenuItem>
-            </Link>
-            <DropdownMenuItem>View payment details</DropdownMenuItem>
+            <DropdownMenuItem className="text-red-500">Delete</DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
-      )
+      );
     },
   },
-]
+];
