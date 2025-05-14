26a0e7b6e65d1bbc6fba4edcb8102e731e6910bf
Alexander Acojido <118715035+sleepypillowz@users.noreply.github.com>
2025-05-14 10:01:07 +0800

    Delete backend directory

--- backend/medicine/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Medicine
-
-
-class MedicineSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Medicine
-        fields = ['id', 'name', 'category', 'dosage_form', 'strength', 'manufacturer', 'indication', 'classification', 'stocks', 'expiration_date']
-
