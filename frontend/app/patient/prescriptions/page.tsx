5140bfbd983463d2f7484412d30823f98f245d7c
Alexander Acojido <alexacojido@gmail.com>
2025-07-30 23:22:41 +0800

    Update patient prescriptions
    used map

--- frontend/app/patient/prescriptions/page.tsx
+++ frontend/app/patient/prescriptions/page.tsx
@@ -10,6 +10,30 @@ import {
 import { CloudDownload, Eye, Trash } from "lucide-react";
 
 export default function PatientPrescriptions() {
+  const prescriptions = [
+    {
+      id: "#A003",
+      title: "Prescription 1",
+      createdby: "Dr. Jacob Ryan",
+      date: "	12/05/2016",
+      disease: "Fever",
+    },
+    {
+      id: "#A002",
+      title: "Prescription 1",
+      createdby: "Dr. Jacob Ryan",
+      date: "	12/05/2016",
+      disease: "Cholera",
+    },
+    {
+      id: "#A001",
+      title: "Prescription 1",
+      createdby: "Dr. Jacob Ryan",
+      date: "	12/05/2016",
+      disease: "Jaundice",
+    },
+  ];
+
   return (
     <div className="card m-6">
       <h1 className="font-bold">Prescription</h1>
@@ -25,63 +49,35 @@ export default function PatientPrescriptions() {
           </TableRow>
         </TableHeader>
         <TableBody>
-          <TableRow>
-            <TableCell>#A348</TableCell>
-            <TableCell>Prescription 1</TableCell>
-            <TableCell>Dr. Jacob Ryan</TableCell>
-            <TableCell>12/05/2016</TableCell>
-            <TableCell>
-              <Badge
-                variant="outline"
-                className="rounded-full border-red-500 text-red-500"
-              >
-                Fever
-              </Badge>
-            </TableCell>
-            <TableCell className="flex space-x-2">
-              <CloudDownload className="h-5 w-5 cursor-pointer text-purple-500 hover:fill-current" />
-              <Eye className="h-5 w-5 cursor-pointer text-green-500 hover:fill-current" />
-              <Trash className="h-5 w-5 cursor-pointer text-red-500 hover:fill-current" />
-            </TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>#A348</TableCell>
-            <TableCell>Prescription 1</TableCell>
-            <TableCell>Dr. Jacob Ryan</TableCell>
-            <TableCell>12/05/2016</TableCell>
-            <TableCell>
-              <Badge
-                variant="outline"
-                className="rounded-full border-green-500 text-green-500"
-              >
-                Cholera
-              </Badge>
-            </TableCell>
-            <TableCell className="flex space-x-2">
-              <CloudDownload className="h-5 w-5 cursor-pointer text-purple-500 hover:fill-current" />
-              <Eye className="h-5 w-5 cursor-pointer text-green-500 hover:fill-current" />
-              <Trash className="h-5 w-5 cursor-pointer text-red-500 hover:fill-current" />
-            </TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell>#A348</TableCell>
-            <TableCell>Prescription 1</TableCell>
-            <TableCell>Dr. Jacob Ryan</TableCell>
-            <TableCell>12/05/2016</TableCell>
-            <TableCell>
-              <Badge
-                variant="outline"
-                className="rounded-full border-purple-500 text-purple-500"
-              >
-                Jaundice
-              </Badge>
-            </TableCell>
-            <TableCell className="flex space-x-2">
-              <CloudDownload className="h-5 w-5 cursor-pointer text-purple-500 hover:fill-current" />
-              <Eye className="h-5 w-5 cursor-pointer text-green-500 hover:fill-current" />
-              <Trash className="h-5 w-5 cursor-pointer text-red-500 hover:fill-current" />
-            </TableCell>
-          </TableRow>
+          {prescriptions.map((item) => (
+            <TableRow key={item.id}>
+              <TableCell>{item.id}</TableCell>
+              <TableCell>{item.title}</TableCell>
+              <TableCell>{item.createdby}</TableCell>
+              <TableCell>{item.date}</TableCell>
+              <TableCell>
+                <Badge
+                  variant="outline"
+                  className={`rounded-full ${
+                    item.disease === "Fever"
+                      ? "border-red-500 text-red-500"
+                      : item.disease === "Cholera"
+                      ? "border-green-500 text-green-500"
+                      : item.disease === "Jaundice"
+                      ? "border-purple-500 text-purple-500"
+                      : item.disease === "Jaundice"
+                  }`}
+                >
+                  {item.disease}
+                </Badge>
+              </TableCell>
+              <TableCell className="flex space-x-2">
+                <CloudDownload className="h-5 w-5 cursor-pointer text-blue-500 hover:fill-current" />
+                <Eye className="h-5 w-5 cursor-pointer text-green-500 hover:fill-current" />
+                <Trash className="h-5 w-5 cursor-pointer text-red-500 hover:fill-current" />
+              </TableCell>
+            </TableRow>
+          ))}
         </TableBody>
       </Table>
     </div>
