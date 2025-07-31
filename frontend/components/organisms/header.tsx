5295c38d68d686d498e5510ddfaaf42801827cf4
Alexander Acojido <alexacojido@gmail.com>
2025-07-31 23:04:06 +0800

    added shadow-md to header and sidebar

--- frontend/components/organisms/header.tsx
+++ frontend/components/organisms/header.tsx
@@ -26,7 +26,7 @@ const Header = () => {
   }, []);
 
   return (
-    <div className="z-50 bg-card text-card-foreground shadow-sm lg:sticky lg:top-0">
+    <div className="z-50 bg-card text-card-foreground shadow-md lg:sticky lg:top-0">
       <div className="px-3 py-3 lg:px-5 lg:pl-3">
         <div className="flex items-center justify-between">
           <div className="flex items-center justify-start rtl:justify-end"></div>
