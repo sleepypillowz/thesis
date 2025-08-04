30d6247d489dcacf4b2bb0f105e66fff7bc76091
Alexander Acojido <alexacojido@gmail.com>
2025-08-04 22:55:07 +0800

    changed font to nunito

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,17 +1,7 @@
 import { ThemeProvider } from "@/utils/theme-provider";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { nunito } from "@/app/fonts";
 
 export const metadata: Metadata = {
   title: "MediTrakk",
@@ -25,9 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${nunito.className} antialiased`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
