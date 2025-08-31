c81673afb2db3a6cfdb551cdb5b158d26df6969e
Alexander Acojido <alexacojido@gmail.com>
2025-08-31 13:30:56 +0800

    Update admin dashboard
    fix total appointments dark mode

--- frontend/app/admin/page.tsx
+++ frontend/app/admin/page.tsx
@@ -2,6 +2,7 @@ import PatientChart from "@/components/organisms/charts/patient-chart";
 import AdminStatCards from "./components/admin-stat-cards";
 import { CalendarFunction } from "./components/calendar";
 import HospitalSurveyChart from "@/components/organisms/charts/hospital-survey-chart";
+import { TotalAppointments } from "./components/total-appointments";
 
 export default function Page() {
   return (
@@ -25,27 +26,7 @@ export default function Page() {
       </div>
       <div className="grid grid-cols-3 gap-4">
         <HospitalSurveyChart />
-        <div className="card flex flex-col justify-evenly p-6 rounded-2xl shadow-md bg-white">
-          {/* Header */}
-          <div className="flex flex-col items-center text-center">
-            <h1 className="text-lg font-semibold text-gray-700">
-              Total Appointments
-            </h1>
-            <span className="text-4xl font-bold text-gray-900 my-4">128</span>
-          </div>
-
-          {/* Stats */}
-          <div className="flex flex-row justify-between gap-4">
-            <div className="flex flex-col items-center flex-1 bg-blue-100 text-blue-600 p-4 rounded-xl shadow-sm">
-              <span className="text-2xl font-bold">73</span>
-              <span className="text-sm font-medium">Completed</span>
-            </div>
-            <div className="flex flex-col items-center flex-1 bg-orange-100 text-orange-600 p-4 rounded-xl shadow-sm">
-              <span className="text-2xl font-bold">55</span>
-              <span className="text-sm font-medium">Upcoming</span>
-            </div>
-          </div>
-        </div>
+        <TotalAppointments />
       </div>
     </div>
   );
