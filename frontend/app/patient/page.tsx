514652569e8db5e31c9e517927437ed33afba41e
Alexander Acojido <alexacojido@gmail.com>
2025-09-05 22:49:47 +0800

    feat(patient/dashboard): display user name

--- frontend/app/patient/page.tsx
+++ frontend/app/patient/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import StatsCard from "@/components/organisms/patient-stats-cards";
 import { PerformanceHeartRateChart } from "@/app/patient/components/performance-heart-rate-chart";
 import { RestingHeartRateChart } from "@/app/patient/components/resting-heart-rate-chart";
@@ -6,12 +8,17 @@ import Image from "next/image";
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { prescriptions, appointments, documents } from "@/lib/placeholder-data";
+
 import { DashboardTable } from "@/components/ui/dashboard-table";
 import { columns } from "./(appointments)/columns";
 import { columns as PrescriptionsColumn } from "./prescriptions/columns";
 import { columns as DocumentsColumn } from "./documents/columns";
+import { useName } from "@/hooks/use-name";
+import { Skeleton } from "@/components/ui/skeleton";
 
-export default async function Page() {
+export default function Page() {
+  const name = useName();
+
   return (
     <div className="m-6 space-y-6">
       <section className="card">
@@ -27,7 +34,9 @@ export default async function Page() {
 
           <div className="col-span-2 flex flex-col content-center space-y-4 p-12">
             <span className="font-semibold">Welcome back</span>
-            <span className="text-2xl font-bold text-blue-500">John Doe!</span>
+            <span className="text-2xl font-bold text-blue-500">
+              {name ?? <Skeleton className="mb-1 h-8 w-[120px]" />}
+            </span>
             <p className="text-muted-foreground">
               We would like to take this opportunity to welcome you to our
               practice and to thank you for choosing our physicians to
