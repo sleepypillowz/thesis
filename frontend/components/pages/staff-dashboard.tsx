4c7010e45948f89807df06f604eae1d82a6beea5
Alexander Acojido <alexacojido@gmail.com>
2025-09-05 19:13:20 +0800

    refactor(staff-dashboard)
    used hook useName, sorted imports, changed p tag to span

--- frontend/components/pages/staff-dashboard.tsx
+++ frontend/components/pages/staff-dashboard.tsx
@@ -1,17 +1,24 @@
 "use client";
+import { useName } from "@/hooks/use-name";
+import { Skeleton } from "../ui/skeleton";
+
+import StatsCard from "@/components/organisms/admin-stats-cards";
 import { VisitorsChart } from "@/components/organisms/charts/visitors-chart";
 import { CommonDiseasesChart } from "@/components/organisms/charts/common-diseases-chart";
-import StatsCard from "@/components/organisms/admin-stats-cards";
-import MedicalRecords from "@/components/pages/medical-records";
 import { CommonMedicinesChart } from "@/components/organisms/charts/common-medicine-chart";
+import MedicalRecords from "@/components/pages/medical-records";
 
 export default function StaffDashboard() {
+  const name = useName();
+
   return (
     <div className="m-6 space-y-4 text-center md:text-left">
       <div className="mx-2 py-4">
-        <div className="flex space-x-2 text-2xl font-bold">
-          <p>Good Day,</p>
-          <p className="text-blue-500">John Doe</p>
+        <div className="flex flex-row space-x-1 text-2xl font-bold">
+          <span>Good Day,</span>
+          <span className="text-blue-500">
+            {name ?? <Skeleton className="mb-1 h-8 w-[120px]" />}
+          </span>
         </div>
 
         <p className="text-sm">
