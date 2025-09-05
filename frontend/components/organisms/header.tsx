a96acdd9953eaf5ca6405e7b5c9a3066cbcd8d6c
Alexander Acojido <alexacojido@gmail.com>
2025-09-05 18:58:38 +0800

    style(header): removed email & adjusted style

--- frontend/components/organisms/header.tsx
+++ frontend/components/organisms/header.tsx
@@ -1,33 +1,18 @@
+import DarkModeToggle from "@/components/molecules/header/dark-mode-toggle";
 import Notification from "@/components/molecules/header/notification-dropdown";
 import Profile from "@/components/molecules/header/profile";
-import Username from "@/components/atoms/username";
-import Link from "next/link";
-import { DarkModeToggle } from "../molecules/header/dark-mode-toggle";
 
 const Header = () => {
   return (
-    <div className="z-50 bg-card text-card-foreground shadow-md lg:sticky lg:top-0">
-      <div className="px-3 py-3 lg:px-5 lg:pl-3">
-        <div className="flex items-center justify-between">
-          <div className="flex items-center justify-start rtl:justify-end">
-            <Link href="/" className="ms-2 flex md:me-24">
-              <span className="ms-2 self-center whitespace-nowrap text-3xl font-bold tracking-tight">
-                Malibiran Medical Clinic
-              </span>
-            </Link>
-          </div>
-          <div className="relative flex items-center space-x-4">
-            <div className="flex items-center space-x-3">
-              <div className="flex flex-col items-end">
-                <Username />
-              </div>
-              <div className="flex items-center space-x-2">
-                <DarkModeToggle />
-                <Notification />
-                <Profile />
-              </div>
-            </div>
-          </div>
+    <div className="sticky top-0 z-50 bg-card p-3 shadow-md">
+      <div className="flex justify-between">
+        <span className="ms-2 whitespace-nowrap text-3xl font-bold tracking-tight">
+          Malibiran Medical Clinic
+        </span>
+        <div className="space-x-1">
+          <DarkModeToggle />
+          <Notification />
+          <Profile />
         </div>
       </div>
     </div>
