911cf8e2460e4f828c612409db67a3e1ce4d15bf
Alexander Acojido <alexacojido@gmail.com>
2025-09-17 02:17:06 +0800

    disabled(eslint):  no unused var and quotes rule

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "no-unused-vars": "off", // disables unused vars check
+      "quotes": "off", // disables single/double quote enforcement (apos)
+    },
+  },
 ];
 
 export default eslintConfig;
