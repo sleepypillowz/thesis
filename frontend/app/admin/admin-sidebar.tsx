b8784dc32a55acc1a11c437a1de97f47beaad2a8
Alexander Acojido <alexacojido@gmail.com>
2025-09-18 19:42:06 +0800

    style(admin-sidebar): removed sidebar footer

--- frontend/app/admin/admin-sidebar.tsx
+++ frontend/app/admin/admin-sidebar.tsx
@@ -3,7 +3,6 @@ import { LayoutDashboard, ChartNoAxesGantt } from "lucide-react";
 import {
   Sidebar,
   SidebarContent,
-  SidebarFooter,
   SidebarGroup,
   SidebarGroupContent,
   SidebarGroupLabel,
@@ -15,7 +14,6 @@ import {
 
 import Link from "next/link";
 import SidebarHeaderProfile from "@/components/atoms/sidebar-header-profile";
-import { NavUser } from "@/components/nav-user";
 const menu_items = [
   {
     title: "Dashboard",
@@ -73,9 +71,6 @@ export function AppSidebar() {
           </SidebarGroupContent>
         </SidebarGroup>
       </SidebarContent>
-      <SidebarFooter>
-        <NavUser user={data.user} />
-      </SidebarFooter>
     </Sidebar>
   );
 }
