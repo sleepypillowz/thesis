5fe41760f9614432d04a150956ec43ed7e59f4c7
Alexander Acojido <alexacojido@gmail.com>
2025-10-07 23:03:24 +0800

    chore: remove unnecessary files

--- frontend/app/designer/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-  return (
-    <div className="card m-6">
-      <span>This page is for testing out designs</span>
-    </div>
-  );
-}
--- frontend/app/templates/component.tsx
+++
@@ -1,9 +0,0 @@
-const Component = () => {
-
-  return (
-    <div>Hello World!</div>
-  );
-
-}
-
-export default Component;
